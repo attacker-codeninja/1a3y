f7e2cb87546626307ebb6f302252ed202084628d
Kirill Zhdanov <storenth@yandex.ru>
2021-12-18 21:10:04 +0700

    Update provisioner with `subfinder` config

--- helpers/axiom-fleet-setup.sh
+++ helpers/axiom-fleet-setup.sh
@@ -8,6 +8,7 @@ scp_provision_script(){
         axiom-exec 'chmod +x /home/op/lists/wordlist-provision.sh'
         axiom-exec '/home/op/lists/wordlist-provision.sh'
         axiom-exec 'sudo ln -s /usr/bin/python3 /usr/bin/python'
+        axiom-scp '/root/.config/subfinder/config.yaml' 'lazy*':'/home/op/.config/subfinder/config.yaml'
     else
         exit 1
     fi
@@ -21,7 +22,6 @@ notification(){
 main() {
     # Entry point
     scp_provision_script
-    1a3y_custom_helpers
     notification
 }
 
