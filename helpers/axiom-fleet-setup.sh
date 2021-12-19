1acc14f9a5b4897b7f5b7d31aaa1ada30c774e4f
Kirill Zhdanov <storenth@yandex.ru>
2021-12-19 18:03:59 +0700

    Simplify axiom fleets setup after droplets are launched

--- helpers/axiom-fleet-setup.sh
+++ helpers/axiom-fleet-setup.sh
@@ -4,11 +4,7 @@
 
 scp_provision_script(){
     # download and wordlist dependencies to the fleet
-    if axiom-scp '/root/1a3y/helpers/wordlist-provision.sh' 'lazy*':'/home/op/'; then
-        axiom-exec 'chmod +x /home/op/lists/wordlist-provision.sh'
-        axiom-exec '/home/op/lists/wordlist-provision.sh'
-        axiom-exec 'sudo ln -s /usr/bin/python3 /usr/bin/python'
-        axiom-scp '/root/.config/subfinder/config.yaml' 'lazy*':'/home/op/.config/subfinder/config.yaml'
+    if axiom-scp '/root/.config/subfinder/config.yaml' 'lazy*':'/home/op/.config/subfinder/config.yaml'; then
     else
         exit 1
     fi
