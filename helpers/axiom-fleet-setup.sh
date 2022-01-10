03c8bb72a5cbbf772b1376158d2db87ebf580f1b
Kirill Zhdanov <storenth@yandex.ru>
2022-01-10 18:26:22 +0700

    Apply style google guide

--- helpers/axiom-fleet-setup.sh
+++ helpers/axiom-fleet-setup.sh
@@ -3,24 +3,24 @@
 # Script works in current directory
 
 scp_provision_script(){
-    # download and wordlist dependencies to the fleet
-    if ! axiom-scp '/root/.config/subfinder/config.yaml' 'lazy*':'/home/op/.config/subfinder/'; then
-        exit 1
-    fi
-    if ! axiom-scp /root/1a3y/resolvers/vps_resolvers.txt 'lazy*':'/home/op/lists/'; then
-        exit 1
-    fi
+  # download and wordlist dependencies to the fleet
+  if ! axiom-scp '/root/.config/subfinder/config.yaml' 'lazy*':'/home/op/.config/subfinder/'; then
+    exit 1
+  fi
+  if ! axiom-scp /root/1a3y/resolvers/vps_resolvers.txt 'lazy*':'/home/op/lists/'; then
+    exit 1
+  fi
 }
 
 notification(){
-    echo
-    echo "axiom's fleet is ready to launch the 1a3y"
+  echo
+  echo "axiom's fleet is ready to launch the 1a3y"
 }
 
 main() {
-    # Entry point
-    scp_provision_script
-    notification
+  # Entry point
+  scp_provision_script
+  notification
 }
 
 main
