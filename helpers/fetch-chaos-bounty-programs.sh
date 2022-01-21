def434efc4f07d0762bf7ff1b44a6dfd219b1625
Kirill Zhdanov <storenth@yandex.ru>
2022-01-21 22:37:34 +0700

    Add randomizer `shuf`

--- helpers/fetch-chaos-bounty-programs.sh
+++ helpers/fetch-chaos-bounty-programs.sh
@@ -7,3 +7,6 @@
 
 NUMBEROFPROGRAMS="$@"
 curl -s https://raw.githubusercontent.com/projectdiscovery/public-bugbounty-programs/master/chaos-bugbounty-list.json | jq -r ".programs[] | select(.bounty==true) .domains[]" | head -n "${NUMBEROFPROGRAMS}"
+
+# use `shuf`` to shuffle to randomize the output
+# curl -s https://raw.githubusercontent.com/projectdiscovery/public-bugbounty-programs/master/chaos-bugbounty-list.json | jq -r ".programs[] | select(.bounty==true) .domains[]" | shuf | head -n "${NUMBEROFPROGRAMS}"
